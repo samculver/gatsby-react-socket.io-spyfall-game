84253541d080673b4a945d83c461e0949f2ee629
Sam <samculver@gmail.com>
2019-06-15 11:44:32 -0700

    Initial commit

--- client/src/pages/lobby.js
+++ client/src/pages/lobby.js
@@ -1,38 +1,49 @@
 import React from "react"
 import { navigate } from "gatsby"
 import styles from "../styles.module.scss"
+import GameContext from "../context/gameContext"
 import Layout from "../components/layout"
 import Button from "@material-ui/core/Button"
 
-const Lobby = () => (
-  <Layout>
-    <h2>Waiting for players</h2>
-    <h3>Access code: 4sfu2i</h3>
-    <div className={styles.playersList}>
-    <h3>Players:</h3>
-      <ul>
-        <li>Player 1</li>
-        <li>Player 2</li>
-        <li>Player 3</li>
-      </ul>
-    </div>
-    <Button
-      onClick={() => navigate("/play/")}
-      variant="outlined"
-      color="primary"
-      className={styles.button}
-    >
-      Start
-    </Button>
-    <Button
-      onClick={() => navigate("/")}
-      variant="outlined"
-      color="primary"
-      className={styles.button}
-    >
-      Leave
-    </Button>
-  </Layout>
-)
+class Lobby extends React.Component {
+  static contextType = GameContext
+
+  state = {
+    users: [],
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h2>Waiting for players</h2>
+        <h3>{`Access code: ${this.context.gameCode}`}</h3>
+        <div className={styles.playersList}>
+          <h3>Players:</h3>
+          <ul>
+            <li>Player 1</li>
+            <li>Player 2</li>
+            <li>Player 3</li>
+          </ul>
+        </div>
+        <Button
+          onClick={() => navigate("/play/")}
+          variant="outlined"
+          color="primary"
+          className={styles.button}
+        >
+          Start
+        </Button>
+        <Button
+          onClick={() => navigate("/")}
+          variant="outlined"
+          color="primary"
+          className={styles.button}
+        >
+          Leave
+        </Button>
+      </Layout>
+    )
+  }
+}
 
 export default Lobby
--- client/src/pages/new.js
+++ client/src/pages/new.js
@@ -1,40 +1,60 @@
-import React from "react"
+import React, { useContext } from "react"
 import { navigate } from "gatsby"
 import styles from "../styles.module.scss"
+import { socket } from "../socket"
 import Layout from "../components/layout"
 import Header from "../components/header"
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
+import GameContext from "../context/gameContext"
 
-const Create = () => (
-  <Layout>
-    <Header />
-    <TextField
-      id="name"
-      label="Your name"
-      className={styles.textField}
-      margin="normal"
-      variant="outlined"
-      autoComplete="off"
-    />
-    <br />
-    <Button
-      onClick={() => navigate("/lobby/")}
-      variant="outlined"
-      color="primary"
-      className={styles.button}
-    >
-      Create
-    </Button>
-    <Button
-      onClick={() => navigate("/")}
-      variant="outlined"
-      color="primary"
-      className={styles.button}
-    >
-      Back
-    </Button>
-  </Layout>
-)
+const Create = () => {
+  const { setNick, setGameCode } = useContext(GameContext)
+  let nameInput = React.createRef()
+
+  const createGame = () => {
+    const nick = nameInput.current.value
+    socket.emit("create nick", nick, () => {
+      console.log(`created nickname: ${nick}`)
+      socket.emit("create game", code => {
+        console.log(`game code created: ${code}`)
+        setGameCode(code);
+        navigate("/lobby/")
+      })
+    })
+  }
+
+  return (
+    <Layout>
+      <Header />
+      <TextField
+        id="name"
+        label="Your name"
+        className={styles.textField}
+        margin="normal"
+        variant="outlined"
+        autoComplete="off"
+        inputRef={nameInput}
+      />
+      <br />
+      <Button
+        onClick={createGame}
+        variant="outlined"
+        color="primary"
+        className={styles.button}
+      >
+        Create
+      </Button>
+      <Button
+        onClick={() => navigate("/lobby/")}
+        variant="outlined"
+        color="primary"
+        className={styles.button}
+      >
+        Back
+      </Button>
+    </Layout>
+  )
+}
 
 export default Create
