9828dfb84c74b7ddb81cebfd4649e118a702b0a1
Sam <samculver@gmail.com>
2019-06-19 16:38:10 -0700

    initializing socket.io from gatsby-browser

--- client/gatsby-browser.js
+++ client/gatsby-browser.js
@@ -1,7 +1,26 @@
 import React from "react"
+import * as io from 'socket.io-client';
 
 import { GameProvider } from "./src/context/gameContext"
 
 export const wrapRootElement = ({ element }) => (
   <GameProvider>{element}</GameProvider>
 )
+
+export const onClientEntry = () => {
+  console.log("Starting socket connection")
+  const socket = io(`ws://localhost:8081`)
+
+  let latestVersion = 0;
+  
+  socket.emit('get version', (version) => {
+    latestVersion = version;
+    console.log('running version:', version);
+  })
+
+  socket.emit("identify", localStorage.getItem("token"), user => {
+    console.log("identified", user)
+    localStorage.setItem("token", user.token)
+    this.setState({ user })
+  })
+}
