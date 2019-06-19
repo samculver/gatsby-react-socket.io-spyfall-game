fa7544a1ef2706fcd5faec3b959fa938d9f92cfe
Sam <samculver@gmail.com>
2019-06-18 22:44:08 -0700

    Initial commit

--- client/src/context/gameContext.js
+++ client/src/context/gameContext.js
@@ -31,7 +31,15 @@ class GameProvider extends React.Component {
     this.setState({ nick })
   }
 
-
+  componentDidMount() {
+    
+    socket.emit('identify', localStorage.getItem('token'), (user) => {
+        console.log('identified', user);
+        localStorage.setItem('token', user.token);
+        this.setState({ user });
+    });
+    
+  }
 
   render() {
     const { children } = this.props
